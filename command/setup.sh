02ff22d9e4d48b9cfc1b35b7a23764899246ebbf
Andy Kirkham <andy.kirkham@logicnow.com>
2017-02-11 22:36:12 +0000

    Add local public key

--- command/setup.sh
+++ command/setup.sh
@@ -6,6 +6,11 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get -y update
 sudo apt-get install -y ansible git
 
+ssh-keygen -t rsa -N "" -f ~/.ssh/aws-sdk-cpp-key
+if [[ -e ~/.ssh/aws-sdk-cpp-key.pub ]]; then
+	cp ~/.ssh/aws-sdk-cpp-key.pub ../ami/docker-host/files/aws-sdk-cpp-key.pub
+fi
+
 echo '==> provisioning with Ansible'
 sudo ansible-playbook playbooks/setup.yml 
 
