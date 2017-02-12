20cbecc8345bfafad402d50f868d060cd44cfc0d
Andy Kirkham <andy.kirkham@logicnow.com>
2017-02-12 22:27:12 +0000

    Add ssh to installer

--- command/setup.sh
+++ command/setup.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
 echo '==> Installing Ansible and Git'
+sudo apt-get -y update
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get -y update
-sudo apt-get install -y ansible git
+
+ENV DEPS \
+	ansible \
+	git \
+	curl \
+	wget \
+	subversion \
+	awscli \
+	tree \
+	jq \
+	build-essential \
+	apt-utils \
+	ca-certificates \
+	libssl-dev \
+	libcurl4-openssl-dev \
+	vim \
+	libc-dev \
+	file \
+	mlocate \
+	libxml2 \
+	autoconf \
+	g++ \
+	gcc \
+	make
+
+RUN apt-get update && apt-get install -y $DEPS $DEPS_TEMP --no-install-recommends
 
 ssh-keygen -t rsa -N "" -f ~/.ssh/aws-sdk-cpp-key
-if [[ -e ~/.ssh/aws-sdk-cpp-key.pub ]]; then
+if [ -e ~/.ssh/aws-sdk-cpp-key.pub ]; then
 	cp ~/.ssh/aws-sdk-cpp-key.pub ../ami/docker-host/files/aws-sdk-cpp-key.pub
 fi
 
